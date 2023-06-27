8292646475a4e21c987bc7b4dd4d2f0c74ea4c25
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-06-27 21:01:45 +1000

    Add register link

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -39,6 +39,10 @@ defineProps({
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                     >Log in</Link
                 >
+                <Link
+                    :href="route('register')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    >Register</Link
             </template>
         </div>
 
